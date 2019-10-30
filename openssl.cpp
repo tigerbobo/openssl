6effc7d872cf71891f397370e281824a3edc0971
tigerbobo <tigerwd890@163.com>
2019-10-30 15:49:24 +0800

    Update openssl.cpp

--- openssl.cpp
+++ openssl.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include "openssl.h"
+#include "OpenSSL.h"
 
 #pragma comment(lib, "libeay32.lib")
 
@@ -13,9 +13,9 @@ COpenSSL::~COpenSSL()
 {
 }
 
-void COpenSSL::openssl_des3(const char *input, int type, int mode, unsigned char key[24], unsigned char ivec[8], char **output)
+void COpenSSL::openssl_des3(const unsigned char *input, unsigned int length_input, int type, int mode, unsigned char key[24], unsigned char ivec[8], unsigned char **output, unsigned int &length_output)
 {
-	size_t i, length, block;
+	size_t i, block;
 	unsigned char buffer_in[8];
 	unsigned char k1[8], k2[8], k3[8];
 	
@@ -29,28 +29,28 @@ void COpenSSL::openssl_des3(const char *input, int type, int mode, unsigned char
 	DES_set_key_unchecked((const_DES_cblock*)k2, &ks2);
 	DES_set_key_unchecked((const_DES_cblock*)k3, &ks3);
 
-	length = ((strlen(input) / 8) + (strlen(input) % 8 ? 1 : 0)) * 8 + 1;
+	length_output = ((length_input / 8) + (length_input % 8 ? 1 : 0)) * 8;
 
-	*output = new char[length];
-	memset(*output, 0, length);
+	*output = new unsigned char[length_output];
+	memset(*output, 0, length_output);
 
 	switch (mode)
 	{
 	case MODE_CBC:
-		DES_ede3_cbc_encrypt((const unsigned char*)input, (unsigned char*)(*output), (long)(strlen(input)), &ks1, &ks2, &ks3, (DES_cblock*)ivec, type);
+		DES_ede3_cbc_encrypt((const unsigned char*)input, (unsigned char*)(*output), length_input, &ks1, &ks2, &ks3, (DES_cblock*)ivec, type);
 			break;
 	case MODE_CFB:
-		DES_ede3_cfb_encrypt((const unsigned char*)input, (unsigned char*)(*output), 8, (long)(strlen(input)), &ks1, &ks2, &ks3, (DES_cblock*)ivec, type);
+		DES_ede3_cfb_encrypt((const unsigned char*)input, (unsigned char*)(*output), 8, length_input, &ks1, &ks2, &ks3, (DES_cblock*)ivec, type);
 		break;
 	default:
-		block = strlen(input) / 8;
+		block = length_input / 8;
 		for (i = 0; i < block; i++)
 		{
 			memcpy_s(buffer_in, sizeof(buffer_in), input + i * 8, sizeof(buffer_in));
 			DES_ecb3_encrypt((const_DES_cblock*)buffer_in, (const_DES_cblock*)(*output + i * 8), &ks1, &ks2, &ks3, type);
 		}
 
-		if (strlen(input) % 8)
+		if (length_input % 8)
 		{
 			memset(buffer_in, 0, sizeof(buffer_in));
 			memcpy_s(buffer_in, sizeof(buffer_in), input + i * 8, sizeof(buffer_in));
@@ -59,4 +59,5 @@ void COpenSSL::openssl_des3(const char *input, int type, int mode, unsigned char
 		break;
 	}
 }
+
 
